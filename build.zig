0cd0c76d578b4f40033b52e570a16b440667b288
Andrew English <aje299@gmail.com>
2024-05-13 07:11:03 -0700

    Convert executable into test

--- build.zig
+++ build.zig
@@ -13,38 +13,15 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     inline for (exec_targets) |exec_target| {
-        const exe = b.addExecutable(.{
+        const exe_tests = b.addTest(.{
             .name = exec_target.name,
             .root_source_file = b.path("src/" ++ exec_target.name ++ "/" ++ exec_target.name ++ ".zig"),
             .target = target,
             .optimize = optimize,
         });
 
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
-
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        const run_step = b.step(exec_target.name, "Run" ++ exec_target.name);
-        run_step.dependOn(&run_cmd.step);
+        const run_tests = b.addRunArtifact(exe_tests);
+        const test_step = b.step(exec_target.name, "Test " ++ exec_target.name);
+        test_step.dependOn(&run_tests.step);
     }
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/heap_sort/heap_sort.zig
+++ src/heap_sort/heap_sort.zig
@@ -5,27 +5,35 @@ fn left_child(i: usize) usize {
 }
 
 fn heapSort(array: []i32) void {
-    var start = array.len / 2;
+    var start = array.len / 2; // leaf nodes are balanced, start at first inner node
     var end = array.len;
 
     while (end > 1) {
         if (start > 0) {
+            // p1: building max heap
             start = start - 1;
         } else {
+            // p2: using built max heap to order array
             end = end - 1;
 
+            // insert max of heap at last unsorted spot in array
             const temp = array[end];
             array[end] = array[0];
             array[0] = temp;
         }
 
+        // balance sub tree of max heap
+        // p1: bottom -> up
+        // p2: heap root replaced -> balance heap
         var root = start;
         while (left_child(root) < end) {
+            // find largest child of left and right nodes
             var child = left_child(root);
             if (child + 1 < end and array[child] < array[child + 1]) {
                 child = child + 1;
             }
 
+            // swap when parent is not ordered properly
             if (array[root] < array[child]) {
                 const temp = array[root];
                 array[root] = array[child];
@@ -39,7 +47,7 @@ fn heapSort(array: []i32) void {
     }
 }
 
-pub fn main() void {
+test heapSort {
     const n = 20;
     const random = std.crypto.random;
 
@@ -49,5 +57,7 @@ pub fn main() void {
     }
 
     heapSort(&array);
-    std.debug.print("Sorted: {any}\n", .{array});
+    for (1..n) |i| {
+        try std.testing.expect(array[i - 1] <= array[i]);
+    }
 }
